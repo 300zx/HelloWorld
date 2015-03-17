1e8a4dd95e88a55a535364de1ecf5d5fc5608cdb
Pavel Razuvayev <pavel.razuvayev@gmail.com>
2015-03-16 20:28:20 -0500

    HelloWorldToast
    One button to rule them all, it shows "Hello World" toast.

---
+++ app/src/main/java/ferz/co/helloworld/MainActivity.java
@@ -0,0 +1,29 @@
+package ferz.co.helloworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.*;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
